03069fa9ae46b0e6bfe133e47c91ebcd5f326f2c
rshiyamdg <arashmohammadi9775@gmail.com>
2026-02-23 19:00:08 -0800

    small typo fix

--- src/utils/Configs.js
+++ src/utils/Configs.js
@@ -352,7 +352,7 @@ export const SVG_DRAW_LAYOUTS = {
 
 // Background configuration per device (customize as needed)
 export const BG_LAYOUTS = {
-  mobile: { position: [0, 0, 0], scale: [5, 5, 1], map: `${BASE}textures/bg/1k/painted_plaster_wall_diff_2k.jpg`, normalMap: `${BASE}textures/bg/1k/painted_plaster_wall_nor_gl_1k.jpg`, roughnessMap: `${BASE}textures/bg/1k/painted_plaster_wall_rough_1k.jpg` },
+  mobile: { position: [0, 0, 0], scale: [5, 5, 1], map: `${BASE}textures/bg/1k/painted_plaster_wall_diff_1k.jpg`, normalMap: `${BASE}textures/bg/1k/painted_plaster_wall_nor_gl_1k.jpg`, roughnessMap: `${BASE}textures/bg/1k/painted_plaster_wall_rough_1k.jpg` },
   tablet: { position: [0, 0, 0], scale: [15, 15, 1], map: `${BASE}textures/bg/1k/painted_plaster_wall_diff_1k.jpg`, normalMap: `${BASE}textures/bg/1k/painted_plaster_wall_nor_gl_1k.jpg`, roughnessMap: `${BASE}textures/bg/1k/painted_plaster_wall_rough_1k.jpg` },
   desktop: { position: [0,0, -1.4], scale: [20, 20,1], map: `${BASE}textures/bg/painted_plaster_wall_diff_2k.jpg`, normalMap: `${BASE}textures/bg/1k/painted_plaster_wall_nor_gl_1k.jpg`, roughnessMap: `${BASE}textures/bg/painted_plaster_wall_rough_2k.png` },
 };
