7996bb3d174e1ac930cd414517759f4f6252faf0
rshiyamdg <arashmohammadi9775@gmail.com>
2026-02-23 20:05:05 -0800

    section3 fix

--- src/utils/section3/ContactSection.jsx
+++ src/utils/section3/ContactSection.jsx
@@ -209,12 +209,12 @@ export function ContactSection() {
       
       <div className="left-column h-full z-10 flex items-center w-full md:w-[75%] overflow-hidden">
         <div className="text-wrapper p-4 pt-10 md:p-16 w-[90%] md:w-[80%] flex flex-col justify-center cursor-default origin-left">
-          <img src={`${import.meta.env.BASE_URL}images/logo.png`} alt="Logo" className="w-16 h-16 md:w-48 md:h-48 mb-4  pointer-events-none" />
+          
           <h1 className="text-xl md:text-5xl sm:text-4xl font-bold text-black max-w-4xl mb-2">ASAL KOJVARZADEH NOBARI</h1>
           <h2 className="text-lg md:text-2xl underline mt-1  font-bold text-black mb-8 inline-block">Architect</h2>
           <div className="mt-2 md:mt-1">
             <h3 className="text-base md:text-xl sm:text-lg font-bold mb-2 text-black/80">About Me</h3>
-            <p className="sm:text-xs lg:text-sm text-black font-bold max-w-full sm:max-w-10xl leading-relaxed">
+            <p className="sm:text-sm lg:text-sm text-black font-bold max-w-full sm:max-w-10xl leading-relaxed">
               I am an architect who is enthusiastic about sustainable architecture. I have a bachelor in Architectural Engineering and I would like to extend my knowledge, career and degree in  this area, My ultimate goal is to bring social attention to this matter and influence more people to act instead of just studying about it. Eventually I love to teach students to keep the information spreading in peoples minds.
             </p>
           </div>
